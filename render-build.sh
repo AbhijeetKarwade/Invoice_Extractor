5e951decbb1160dd506c4b558a23e8316bbfbaac
unknown <abhijeet.kar158@gmail.com>
2025-07-02 11:53:44 +0530

    Fix setuptools.build_meta error with explicit installs and pandas 1.5.3

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 apt-get update
 apt-get install -y python3-dev build-essential libxml2-dev libxslt1-dev zlib1g-dev
+pip install --no-cache-dir setuptools==74.1.2 wheel==0.44.0
 pip install --no-cache-dir --only-binary :all: -r requirements.txt
