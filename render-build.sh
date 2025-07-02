30f3a000e57282ecfc406e8ed7eb414525636323
unknown <abhijeet.kar158@gmail.com>
2025-07-02 12:25:10 +0530

    newly

--- render-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install -y python3-dev build-essential libxml2-dev libxslt1-dev zlib1g-dev
-pip install --no-cache-dir setuptools==68.0.0 wheel==0.44.0
-pip install --no-cache-dir --only-binary :all: -r requirements.txt
