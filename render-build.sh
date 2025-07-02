b6d6f153bab209de0327f4e509473bf5de6ece5b
unknown <abhijeet.kar158@gmail.com>
2025-07-02 11:45:52 +0530

    Force binary wheels for pandas installation

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get update
-apt-get install -y python3-dev build-essential
-pip install -r requirements.txt
+apt-get install -y python3-dev build-essential libxml2-dev libxslt1-dev zlib1g-dev
+pip install --no-cache-dir --only-binary :all: -r requirements.txt
